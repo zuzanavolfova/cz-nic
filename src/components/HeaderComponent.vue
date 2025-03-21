ea45452bdf05952b957d12700286808de020a0f6
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-03-21 12:09:02 +0100

    001 edit header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
+import UserHeaderComponent from './UserHeaderComponent.vue';
 
 </script>
 
 <template>
- <div>Header</div>
+ <div class="header">
+    <h1>Ferda</h1>
+<UserHeaderComponent></UserHeaderComponent>
+
+
+
+ </div>
 </template>
+<style scoped lang="scss">
+    .header {
+        background-color: var(--blue-primary);
+        padding: 6px 8px;
+        color: white
+    }
+</style>
