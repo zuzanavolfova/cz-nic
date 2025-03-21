217e6dfd5f13231d87dffe0ed4c8e114120e5bf2
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-03-21 12:09:20 +0100

    001 add use header component

---
+++ src/components/UserHeaderComponent.vue
@@ -0,0 +1,62 @@
+Fixed Vue Menu Component with TypeScript
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { useStore } from './../stores/main';
+
+import Menu from 'primevue/menu';
+import LogInDialog from './Dialogs/LogInDialog.vue';
+
+import type { MenuItem } from 'primevue/menuitem';
+import type { Ref } from 'vue';
+
+
+const store = useStore();
+const userName = computed(() => store.user?.name);
+const userRole = computed(() => store.user?.role);
+
+const items: Ref<MenuItem[]> = ref([
+  { 
+    label: () => `${userName.value}  ${userRole.value}`, 
+    icon: 'pi pi-user', 
+    command: () => console.log('TODO action redirect to user profile') 
+  },
+  { 
+    label: 'Logout', 
+    icon: 'pi pi-sign-out', 
+    command: () => console.log('TODO action log out') 
+  }
+]);
+
+const menu: Ref<null | {toggle: (event: MouseEvent) => void}> = ref(null);
+const openUserMenu = (event: MouseEvent): void => {
+  menu.value?.toggle(event);
+};
+</script>
+
+<template>
+  <div class="user-header">
+    <button @click="openUserMenu" class="p-button user-header-button">
+      <i class="pi pi-user"></i>
+      
+      <span v-if="userName !== null">{{ userName }}</span>
+      <span v-else>Log in</span>
+    </button>
+    <Menu v-if="userName !== null" ref="menu" :model="items" :popup="true" />
+    <LogInDialog v-else></LogInDialog>
+  </div>
+</template>
+
+<style scoped lang=scss>
+.user-header-button {
+    background-color: var(--blue-primary);
+    border: none;
+    color: white;    
+}
+.user-header-button:active {
+    background-color: var(--blue-primary); 
+}
+.p-button:not(:disabled):hover {
+    background-color: var(--blue-primary); 
+    border: none
+}
+</style>
