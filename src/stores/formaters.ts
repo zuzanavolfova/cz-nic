a83ec845844c11f3d86d2a2d42fb87eabe3909c2
ADS\urvozu <zuzana.volfova@rieter.com>
2025-03-24 16:30:46 +0100

    001 add date formater

---
+++ src/stores/formaters.ts
@@ -0,0 +1,17 @@
+export const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    };
+
+    const formattedDate: string = date.toLocaleString('en-US', { ...options, timeZone: 'UTC' });
+
+    return formattedDate.replace(',', '');
+}
