c0c2c04f82a7b9f4de271b8924a2ff3dde65f466
ADS\urvozu <zuzana.volfova@rieter.com>
2025-03-24 09:11:49 +0100

    001 add toggle area to detail layout

--- src/views/DetailLayout.vue
+++ src/views/DetailLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
+import ToggleSwitch from 'primevue/toggleswitch';
 
 defineProps({
   title: {
@@ -11,6 +12,10 @@ defineProps({
 <template>
     <div class="detail-layout">
         <h2> {{ title }}</h2>
+        <div class="detail-layout__toggle">
+            <ToggleSwitch v-model="checked" />
+            <span class="detail-layout__toggle__title">Verbose view</span>
+        </div>
         <div class="detail-layout__content">
             <div class="detail-layout__content__main"></div>
             <div class="detail-layout__content__info"></div>
@@ -22,6 +27,14 @@ defineProps({
         width: 100%;
         padding: 20px;
         background-color: var(--medium-background);
+        &__toggle {
+            display: flex;
+            align-items: center;
+            &__title{
+                color: var(--text-extra-light-grey);
+                margin-left: 16px;
+            }
+        }
         &__content {
             display: flex;
             flex-wrap: wrap;
@@ -34,4 +47,13 @@ defineProps({
             }
         }
     }
+
+    :deep(.p-toggleswitch) {
+        &.p-toggleswitch-checked {
+            .p-toggleswitch-slider {
+                background-color: var(--blue-primary) !important;
+            }
+        }
+    }
+    
 </style>
