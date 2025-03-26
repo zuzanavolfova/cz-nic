09c9b39123da1181f9b462bf149f6e256653e8bd
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-03-26 17:25:02 +0100

    001 add set card component

---
+++ src/components/Cards/SetCart.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { defineProps, ref, onMounted, computed} from 'vue';
+import { useStore } from './../../stores/main';
+import Card from 'primevue/card';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  item: {
+    type: Object,
+    required: true
+  },
+
+});
+</script>
+<template>
+    <Card>
+        <template #title>
+            <div class="administrative-card__title">
+                <h3>{{ title }}</h3>
+            </div>
+        </template>
+        <template #content>
+            <div class="administrative-card__container">
+                <div class="administrative-card__row">
+                    <span style="grid-area: title; font-weight: 600"> Handle: </span>
+                    <span style="grid-area: value; color: var(--blue-primary)"> {{ item.handle}} </span>
+                </div>
+                <div class="administrative-card__row">
+                    <span style="grid-area: title; font-weight: 600"> Registrar: </span>
+                    <span style="grid-area: value; color: var(--blue-primary)"> {{ item.registrar}} </span>
+                </div>
+                <slot></slot>
+            </div>
+        </template>  
+    </Card>
+</template>
+<style lang="scss" scoped>
+     @import "@/assets/styles/cards"; 
+
+    .administrative-card {
+        &__title {
+            @include card-title
+        }
+        &__row {
+            @include card-row-no-icon
+        }
+}
+</style>
